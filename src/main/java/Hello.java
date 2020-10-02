f40a97f587dcdc6a37aea225cda0436fc42cc649
zzambezii <zambezide@gmail.com>
2020-10-02 23:18:16 +0200

    initial commit second change

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,6 +1,9 @@
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Hello class e u3");
+
+        //more changes are coming
+        //i m ready to commit
     }
 }
